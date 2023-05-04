a73658a21688022d5edfcf252b92fb5e12c9b687
Maxim Ichetovkin <ichetovkin.maxim@gmail.com>
2023-05-04 23:37:12 +0200

    fix: добавить семантический тег nav

--- src/components/app-header/AppHeader.js
+++ src/components/app-header/AppHeader.js
@@ -5,22 +5,24 @@ export default function AppHeader() {
 
     return (
         <header className={headerStyles.menu}>
-            <ul className={headerStyles.list}>
-                <div className={headerStyles.group}>
+            <nav>
+                <ul className={headerStyles.list}>
+                    <div className={headerStyles.group}>
+                        <li className={headerStyles.item}>
+                            <BurgerIcon type="primary" />
+                            <p className={headerStyles.title + " text text_type_main-default"}>Конструктор</p>
+                        </li>
+                        <li className={headerStyles.item}>
+                            <ListIcon type="secondary" />
+                            <p className={headerStyles.title + " text text_type_main-default"}>Лента заказов</p>
+                        </li>
+                    </div>
                     <li className={headerStyles.item}>
-                        <BurgerIcon type="primary" />
-                        <p className={headerStyles.title + " text text_type_main-default"}>Конструктор</p>
+                        <ProfileIcon type="secondary" />
+                        <p className={headerStyles.title + " text text_type_main-default"}>Личный кабинет</p>
                     </li>
-                    <li className={headerStyles.item}>
-                        <ListIcon type="secondary" />
-                        <p className={headerStyles.title + " text text_type_main-default"}>Лента заказов</p>
-                    </li>
-                </div>
-                <li className={headerStyles.item}>
-                    <ProfileIcon type="secondary" />
-                    <p className={headerStyles.title + " text text_type_main-default"}>Личный кабинет</p>
-                </li>
-            </ul>
+                </ul>
+            </nav>
             <div className={headerStyles.logo}><Logo /></div>
         </header>
     )
